0ef74e9c910e1f7581bb4bc02d719af7bc862021
WIN_NT\x16j053 <x16j053@chiba-fjb.ac.jp>
2017-05-23 12:29:41 +0900

    コンソール入力

---
+++ SampleC++1-3.cpp
@@ -0,0 +1,16 @@
+// SampleC++1-3.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	cout << "a=" << a << endl;
+    return 0;
+}
+
